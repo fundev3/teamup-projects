93cb33ff803274ba0ea0d59d9b2e392deb44a4e1
ela-ocana-jalafund <Ela.Ocana@fundacion-jala.org>
2021-07-21 21:19:46 -0400

    Add Contact in Models

---
+++ src/models/Jalasoft.TeamUp.Projects.Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace Jalasoft.TeamUp.Projects.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Contact
+    {
+        public Guid Id { get; set; }
+
+        public Guid IdResume { get; set; }
+
+        public string Name { get; set; }
+    }
+}
