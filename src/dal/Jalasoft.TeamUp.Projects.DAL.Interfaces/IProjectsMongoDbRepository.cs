edb275f4884e181e79fafd9abdfd91537147cf39
Casfer98 <alejandrocastellonfer@gmail.com>
2021-07-27 16:23:01 -0400

    add ongodb repository

---
+++ src/dal/Jalasoft.TeamUp.Projects.DAL.Interfaces/IProjectsMongoDbRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Jalasoft.TeamUp.Projects.DAL.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using Jalasoft.TeamUp.Projects.Models;
+
+    public interface IProjectsMongoDbRepository : IRepository<Project>
+    {
+    }
+}
---
+++ src/dal/Jalasoft.TeamUp.Projects.DAL/ProjectsMongoDbRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Jalasoft.TeamUp.Projects.DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using Jalasoft.TeamUp.Projects.DAL.Interfaces;
+    using Jalasoft.TeamUp.Projects.Models;
+
+    public class ProjectsMongoDbRepository : IProjectsMongoDbRepository
+    {
+        public Project Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Project> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
