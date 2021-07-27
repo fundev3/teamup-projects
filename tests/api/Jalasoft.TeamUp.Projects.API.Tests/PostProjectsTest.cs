c7312f0b20e5b890de0cef884607c64df37d06ce
ela-ocana-jalafund <Ela.Ocana@fundacion-jala.org>
2021-07-27 19:47:48 -0400

    Add test of PostProject

--- tests/api/Jalasoft.TeamUp.Projects.API.Tests/PostProjectsTest.cs
+++ tests/api/Jalasoft.TeamUp.Projects.API.Tests/PostProjectsTest.cs
@@ -1,16 +1,11 @@
 namespace Jalasoft.TeamUp.Projects.API.Tests
 {
-    using System;
-    using System.IO;
-    using System.Text;
-    using System.Threading.Tasks;
     using Jalasoft.TeamUp.Projects.API.Controllers;
     using Jalasoft.TeamUp.Projects.Core.Interfaces;
     using Jalasoft.TeamUp.Projects.Models;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Moq;
-    using Newtonsoft.Json;
     using Xunit;
 
     public class PostProjectsTest
@@ -27,8 +22,13 @@ namespace Jalasoft.TeamUp.Projects.API.Tests
         }
 
         [Fact]
-        public void PostProject_Returns_OkObjectResult_Project()
+        public async void PostProject_Returns_OkObjectResult_Project()
         {
+            var request = this.mockHttpContext.Request;
+            this.mockProjectsService.Setup(service => service.PostProject(null)).Returns(new Project());
+            var response = await this.postProject.CreateProject(request);
+            var okObjectResult = Assert.IsType<OkObjectResult>(response);
+            Assert.IsType<Project>(okObjectResult.Value);
         }
     }
 }
