286b5f8733ca76501763dfd38bbae355ec97bdc3
Pedro Rene Lima Camacho <Pedro.Lima@fundacion-jala@org>
2021-07-29 16:17:26 -0400

    merge 149-projects-list-db into 150-create-project-bd

--- src/models/Jalasoft.TeamUp.Projects.Models/Project.cs
+++ src/models/Jalasoft.TeamUp.Projects.Models/Project.cs
@@ -6,16 +6,12 @@
 
     public class Project
     {
-        [BsonRequired]
         public Guid Id { get; set; }
 
-        [BsonRequired]
         public string Name { get; set; }
 
-        [BsonRequired]
         public string Description { get; set; }
 
-        [BsonRequired]
         public Contact Contact { get; set; }
 
         public bool State { get; set; }
