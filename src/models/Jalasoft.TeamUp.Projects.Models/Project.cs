904044d7240555aee17c80bb4565462c5f6fb191
Paulo Terán <paulo.teran@fundacion-jala.org>
2021-07-29 16:06:51 -0400

    GetAll Fully Working

--- src/models/Jalasoft.TeamUp.Projects.Models/Project.cs
+++ src/models/Jalasoft.TeamUp.Projects.Models/Project.cs
@@ -6,32 +6,22 @@
 
     public class Project
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.String)]
         public Guid Id { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
         public string Name { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
         public string Description { get; set; }
 
-        [BsonRepresentation(BsonType.Document)]
         public Contact Contact { get; set; }
 
-        [BsonRepresentation(BsonType.Boolean)]
         public bool State { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
         public string TextInvitation { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
         public string Logo { get; set; }
 
-        [BsonRepresentation(BsonType.Array)]
         public Contact[] MemberList { get; set; }
 
-        [BsonRepresentation(BsonType.DateTime)]
         public DateTime CreationDate { get; set; }
     }
 }
