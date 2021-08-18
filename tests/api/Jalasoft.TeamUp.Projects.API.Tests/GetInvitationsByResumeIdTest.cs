5b3d067c1da844c9a2207d449941cf111e767e6c
Pedro Rene Lima Camacho <Pedro.Lima@fundacion-jala@org>
2021-08-18 11:06:06 -0400

    Modify Interfaces of Projects

---
+++ tests/api/Jalasoft.TeamUp.Projects.API.Tests/GetInvitationsByResumeIdTest.cs
@@ -0,0 +1,39 @@
+﻿namespace Jalasoft.TeamUp.Projects.API.Tests
+{
+    using Jalasoft.TeamUp.Projects.API.Controllers;
+    using Jalasoft.TeamUp.Projects.Core.Interfaces;
+    using Jalasoft.TeamUp.Projects.Models;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using Xunit;
+
+    public class GetInvitationsByResumeIdTest
+    {
+        private readonly Mock<IInvitationsService> mockService;
+        private readonly DefaultHttpContext mockHttpContext;
+        private readonly GetInvitationsByResumeId getInvitations;
+
+        public GetInvitationsByResumeIdTest()
+        {
+            this.mockService = new Mock<IInvitationsService>();
+            this.mockHttpContext = new DefaultHttpContext();
+            this.getInvitations = new GetInvitationsByResumeId(this.mockService.Object);
+        }
+
+        [Fact]
+        public void GetInvitationsByResumeId_Returns_OkObjectResult()
+        {
+            // Arrange
+            var request = this.mockHttpContext.Request;
+            this.mockService.Setup(service => service.GetInvitationsByResumeId(2)).Returns(new Invitation[]());
+
+            // Act
+            var response = this.getInvitations.Run(request, 2);
+
+            // Assert
+            var okObjectResult = Assert.IsType<OkObjectResult>(response);
+            Assert.IsType<Project[]>(okObjectResult.Value);
+        }
+    }
+}
